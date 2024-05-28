d0aa6725fb6a9e8a139b62edb119501a245df4e4
Adamnson-0dinson <theawakeavatar@gmail.com>
2024-05-28 12:07:37 +0530

    add supressions for rubocop offences

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # step 1 : identify the characters that lie between [a-z] => [97,122] and [A-Z] => [65,90]
 # step 2: add shift to all characters in this range -> should lead to overflow
 # step 3: loop back overflow with alphabet_size = 26
@@ -8,11 +10,11 @@ SMALL_A_CODE = 97
 SMALL_Z_CODE = 122
 ALPAHBET_SIZE = 26
 
+# rubocop: disable Metrics/MethodLength
+# rubocop: disable Metrics/AbcSize
 def caesar_cipher(string_to_manipulate, cipher_shift)
   changed_string = ''
-  puts string_to_manipulate
   string_array = string_to_manipulate.bytes # create ASCII character code array
-  # p string_array
   string_array.each_with_index do |char_code, idx|
     if (SMALL_A_CODE..SMALL_Z_CODE).include?(char_code)
       string_array[idx] += cipher_shift
@@ -22,11 +24,12 @@ def caesar_cipher(string_to_manipulate, cipher_shift)
       string_array[idx] += cipher_shift
       string_array[idx] -= ALPAHBET_SIZE if char_code >= BIG_Z_CODE + 1 - cipher_shift
     end
-
+    changed_string += string_array[idx].chr
   end
-  # p string_array
-  puts changed_string
+  puts(string_to_manipulate, "\n", changed_string)
 end
+# rubocop: enable Metrics/MethodLength
+# rubocop: enable Metrics/AbcSize
 
 test_string = 'abcde fghij klmno pqrst uvwxy z'
 test_s = 'The Magic OF BEing a MaN'
